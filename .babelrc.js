902db009f5e65b5ad582bae657790b5c68e69018
Rita Rui <rita.rui@qantasloyalty.com>
2022-11-24 17:04:23 +1100

    OMP - Initial commit

---
+++ .babelrc.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    node: true,
+                },
+            },
+        ],
+        [
+            '@babel/preset-typescript',
+            {
+                onlyRemoveTypeImports: true,
+            },
+        ],
+    ],
+};
